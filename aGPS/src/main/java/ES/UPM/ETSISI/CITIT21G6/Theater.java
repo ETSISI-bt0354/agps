aac7c887943e06ec071dd81f8568e0d4fa67c460
Jorge Benjumea Burillo <jorge.benjumea@alumnos.upm.es>
2023-10-18 12:24:02 +0200

    Theatre --> Theater

--- aGPS/src/main/java/ES/UPM/ETSISI/CITIT21G6/Theater.java
+++ aGPS/src/main/java/ES/UPM/ETSISI/CITIT21G6/Theater.java
@@ -2,18 +2,18 @@ package ES.UPM.ETSISI.CITIT21G6;
 
 import java.util.*;
 
-public class Theatre extends Activities
+public class Theater extends Activities
 {
     private final int MAXIMUMYOUNGAGE = 25;
     private final int MINIMUMELDERLYAGE = 65;
     private final Double YOUNGDISCOUNT = 0.5;
     private final Double ELDERLYDISCOUNT = 0.7;
 
-    public Theatre(String name, String description, int duration, Double cost, OptionalInt capacity)
+    public Theater(String name, String description, int duration, Double cost, OptionalInt capacity)
     {
         super(name, description, duration, cost, capacity);
     }
-    public Theatre(String name, String description, int duration, Double cost)
+    public Theater(String name, String description, int duration, Double cost)
     {
         super(name, description, duration, cost);
     }
