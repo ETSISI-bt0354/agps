be9dc75dc463a1675a9bdc9a418bbf7236e121f4
Miguel Ángel <ma.serrano.sanchez@alumnos.upm.es>
2023-10-20 14:39:21 +0200

    Check score is valid

--- aGPS/src/main/java/ES/UPM/ETSISI/CITIT21G6/model/Ticket.java
+++ aGPS/src/main/java/ES/UPM/ETSISI/CITIT21G6/model/Ticket.java
@@ -4,6 +4,8 @@ import java.util.OptionalInt;
 
 public class Ticket
 {
+    private static final int MINIMUMSCORE = 0;
+    private static final int MAXIMUMSCORE = 10;
     private final User user;
     private final SocialPlan socialPlan;
     private OptionalInt score;
@@ -35,8 +37,11 @@ public class Ticket
         return score;
     }
 
-    public void setScore(OptionalInt score)
+    public void setScore(OptionalInt score) throws Exception
     {
+        if (score.isPresent() && (score.getAsInt() < MINIMUMSCORE || score.getAsInt() > MAXIMUMSCORE))
+            throw new Exception("The core must be between 0 and 10");
+
         this.score = score;
     }
 
