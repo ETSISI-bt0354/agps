3318ec169608d968d344fa9830e3dad61068afb1
Miguel Ángel <ma.serrano.sanchez@alumnos.upm.es>
2023-11-24 22:16:40 +0100

    Remove unused imports

--- aGPS/src/main/java/ES/UPM/ETSISI/CITIT21G6/model/User.java
+++ aGPS/src/main/java/ES/UPM/ETSISI/CITIT21G6/model/User.java
@@ -1,10 +1,8 @@
 package ES.UPM.ETSISI.CITIT21G6.model;
 
 import java.time.LocalDate;
-import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class User
 {
