89d3a88a4743e5a56397f25dd25f88ae5ce1b27b
Jorge Benjumea Burillo <me@intron014.com>
2023-11-26 23:27:10 +0100

    Stuck in testing purgatory

--- aGPS/src/test/java/ES/UPM/ETSISI/CITIT21G6/controller/SocialPlanControllerTest.java
+++ aGPS/src/test/java/ES/UPM/ETSISI/CITIT21G6/controller/SocialPlanControllerTest.java
@@ -1,22 +1,28 @@
 package ES.UPM.ETSISI.CITIT21G6.controller;
 
+import ES.UPM.ETSISI.CITIT21G6.exception.UserException.InvalidAgeException;
+import ES.UPM.ETSISI.CITIT21G6.exception.UserException.InvalidPasswordException;
+import ES.UPM.ETSISI.CITIT21G6.exception.UserException.InvalidPhoneNumberException;
 import ES.UPM.ETSISI.CITIT21G6.model.SocialPlan;
+import ES.UPM.ETSISI.CITIT21G6.model.User;
 import ES.UPM.ETSISI.CITIT21G6.repository.InMemorySocialPlanRepository;
 import ES.UPM.ETSISI.CITIT21G6.repository.SocialPlanRepository;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 import java.util.OptionalInt;
 
 
 public class SocialPlanControllerTest {
 
     @Test
-    void createSocialPlan()
-    {
-        SocialPlan socialPlan = new SocialPlan("test", "test", null, "test");
+    void createSocialPlan() throws InvalidAgeException, InvalidPhoneNumberException, InvalidPasswordException {
+        User loggedUser = new User("test", "test", "test", "test", "test", "test", "test", "test");
         SocialPlanRepository repository = new InMemorySocialPlanRepository();
         SocialPlanView view = new SocialPlanView();
         SocialPlanController controller = new SocialPlanController(repository, view);
+        controller.createSocialPlan(new String[]{"test", "test", "test"});
         String[] args = {"test", "test", "test"};
         assert controller.createSocialPlan(args).isEmpty();
 
@@ -25,7 +31,7 @@ public class SocialPlanControllerTest {
     @Test
     void deleteSocialPlan()
     {
-        SocialPlan socialPlan = new SocialPlan("test", "test", null, "test");
+        SocialPlan socialPlan = new SocialPlan("test", "test", LocalDate.now(), "test");
         SocialPlanRepository repository = new InMemorySocialPlanRepository();
         SocialPlanView view = new SocialPlanView();
         SocialPlanController controller = new SocialPlanController(repository, view);
@@ -36,7 +42,7 @@ public class SocialPlanControllerTest {
     @Test
     void addActivity()
     {
-        SocialPlan socialPlan = new SocialPlan("test", "test", null, "test");
+        SocialPlan socialPlan = new SocialPlan("test", "test", LocalDate.now(), "test");
         SocialPlanRepository repository = new InMemorySocialPlanRepository();
         SocialPlanView view = new SocialPlanView();
         SocialPlanController controller = new SocialPlanController(repository, view);
@@ -47,7 +53,7 @@ public class SocialPlanControllerTest {
     @Test
     void removeNonExistantUser()
     {
-        SocialPlan socialPlan = new SocialPlan("test", "test", null, "test");
+        SocialPlan socialPlan = new SocialPlan("test", "test", LocalDate.now(), "test");
         SocialPlanRepository repository = new InMemorySocialPlanRepository();
         SocialPlanView view = new SocialPlanView();
         SocialPlanController controller = new SocialPlanController(repository, view);
@@ -58,7 +64,7 @@ public class SocialPlanControllerTest {
     @Test
     void removeExistantUser()
     {
-        SocialPlan socialPlan = new SocialPlan("test", "test", null, "test");
+        SocialPlan socialPlan = new SocialPlan("test", "test", LocalDate.now(), "test");
         SocialPlanRepository repository = new InMemorySocialPlanRepository();
         SocialPlanView view = new SocialPlanView();
         SocialPlanController controller = new SocialPlanController(repository, view);
@@ -70,7 +76,7 @@ public class SocialPlanControllerTest {
     @Test
     void addUser()
     {
-        SocialPlan socialPlan = new SocialPlan("test", "test", null, "test");
+        SocialPlan socialPlan = new SocialPlan("test", "test", LocalDate.now(), "test");
         SocialPlanRepository repository = new InMemorySocialPlanRepository();
         SocialPlanView view = new SocialPlanView();
         SocialPlanController controller = new SocialPlanController(repository, view);
