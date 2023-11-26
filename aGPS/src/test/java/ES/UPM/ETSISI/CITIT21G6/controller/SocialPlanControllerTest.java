687d9f41b87a63bba837559ad8e855d4e2cdd8f5
Jorge Benjumea Burillo <me@intron014.com>
2023-11-26 20:43:10 +0100

    tests a beginning part 3

---
+++ aGPS/src/test/java/ES/UPM/ETSISI/CITIT21G6/controller/SocialPlanControllerTest.java
@@ -0,0 +1,52 @@
+package ES.UPM.ETSISI.CITIT21G6.controller;
+
+import ES.UPM.ETSISI.CITIT21G6.repository.SocialPlanRepository;
+import ES.UPM.ETSISI.CITIT21G6.view.SocialPlanView;
+
+import org.junit.jupiter.api.Test;
+import java.util.OptionalInt;
+
+
+public class SocialPlanControllerTest {
+    @Test
+    void createSocialPlan() throws Exception
+    {
+
+    }
+
+    @Test
+    void deleteSocialPlan() throws Exception
+    {
+
+    }
+
+    @Test
+    void addActivity() throws Exception
+    {
+
+    }
+
+    @Test
+    void checkPlanCost() throws Exception
+    {
+
+    }
+
+    @Test
+    void removeUser() throws Exception
+    {
+
+    }
+
+    @Test
+    void addUser() throws Exception
+    {
+
+    }
+
+    @Test
+    void listSocialPlans() throws Exception
+    {
+
+    }
+}
---
+++ aGPS/src/test/java/ES/UPM/ETSISI/CITIT21G6/controller/UserControllerTest.java
@@ -0,0 +1,8 @@
+package ES.UPM.ETSISI.CITIT21G6.controller;
+
+import org.junit.jupiter.api.Test;
+import java.util.OptionalInt;
+
+
+public class UserControllerTest {
+}
