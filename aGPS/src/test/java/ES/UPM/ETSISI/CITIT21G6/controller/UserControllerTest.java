74a2b294635958de7ac1112f727636c56cada7ad
Miguel Ángel <ma.serrano.sanchez@alumnos.upm.es>
2023-11-27 01:35:44 +0100

    UserControllerTest

--- aGPS/src/test/java/ES/UPM/ETSISI/CITIT21G6/controller/UserControllerTest.java
+++ aGPS/src/test/java/ES/UPM/ETSISI/CITIT21G6/controller/UserControllerTest.java
@@ -1,23 +1,67 @@
 package ES.UPM.ETSISI.CITIT21G6.controller;
 
 import ES.UPM.ETSISI.CITIT21G6.model.User;
+import ES.UPM.ETSISI.CITIT21G6.repository.InMemoryUserRepository;
 import ES.UPM.ETSISI.CITIT21G6.repository.UserRepository;
 import ES.UPM.ETSISI.CITIT21G6.view.UserView;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class UserControllerTest
 {
-    UserView view = new ES.UPM.ETSISI.CITIT21G6.controller.UserView();
-    UserRepository repository;
-
-    UserController controller = new UserController(repository, view);
     @Test
     void registerUser()
     {
-        String argUser = "a,12345,2018-12-27,123456789";
-        controller.registerUser(argUser.split(","));
-        controller.registerUser(argUser.split(","));
+        UserRepository repository = new InMemoryUserRepository();
+        UserController controller = new UserController(repository, new UserViewTest());
+
+        controller.registerUser(new String[] {"a", "12345", LocalDate.now().minusYears(30).toString(), "123456789"});
+        assertDoesNotThrow(() -> repository.findByName("a"));
+
+
+        controller.registerUser(new String[] {"b", "12345", LocalDate.now().minusYears(30).toString(), "987654321"});
+        assertDoesNotThrow(() -> repository.findByName("b"));
+    }
+
+    @Test
+    void registedDuplicateUser()
+    {
+        UserRepository repository = new InMemoryUserRepository();
+        UserController controller = new UserController(repository, new UserViewTest());
+
+        controller.registerUser(new String[] {"a", "12345", LocalDate.now().minusYears(30).toString(), "123456789"});
+
+        String result = controller.registerUser(new String[] {"a", "12345", LocalDate.now().minusYears(30).toString(), "123456789"});
+        assertEquals("userAlreadyAdded", result);
+    }
+
+    @Test
+    void registedDuplicateName()
+    {
+        UserRepository repository = new InMemoryUserRepository();
+        UserController controller = new UserController(repository, new UserViewTest());
+
+        controller.registerUser(new String[] {"a", "12345", LocalDate.now().minusYears(30).toString(), "123456789"});
+
+        String result = controller.registerUser(new String[] {"a", "12345", LocalDate.now().minusYears(30).toString(), "987654321"});
+        assertEquals("userAlreadyAdded", result);
+    }
+
+    @Test
+    void registedDuplicatePhone()
+    {
+        UserRepository repository = new InMemoryUserRepository();
+        UserController controller = new UserController(repository, new UserViewTest());
+
+        controller.registerUser(new String[] {"a", "12345", LocalDate.now().minusYears(30).toString(), "123456789"});
+
+        String result = controller.registerUser(new String[] {"b", "12345", LocalDate.now().minusYears(30).toString(), "123456789"});
+        assertEquals("userAlreadyAdded", result);
     }
 
 }
