50cd23b7674c41768314255f6ee27ad4ce3ffaac
Miguel Ángel <ma.serrano.sanchez@alumnos.upm.es>
2024-01-02 19:09:32 +0100

    CommandHandler

---
+++ aGPS/src/main/java/ES/UPM/ETSISI/CITIT21G6/CLI/CommandHandler.java
@@ -0,0 +1,62 @@
+package ES.UPM.ETSISI.CITIT21G6.CLI;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CommandHandler implements Command
+{
+    private Map<String, Command> commands;
+    private String description;
+
+    public CommandHandler(String description)
+    {
+        this.commands = new HashMap<>();
+        this.description = description;
+    }
+
+    public void addCommand(String commandName, Command command)
+    {
+        commands.put(commandName, command);
+    }
+
+    public String exec(List<String> args)
+    {
+        if (args.isEmpty())
+            return help();
+
+        String commandName = args.get(0);
+        args.remove(0);
+
+        Command command = commands.get(commandName);
+        if (command == null)
+        {
+            StringBuilder message = new StringBuilder();
+            message.append(commandName);
+            message.append(" is not a command\n");
+            message.append(help());
+            return message.toString();
+        }
+
+        return command.exec(args);
+    }
+
+    public String description()
+    {
+        return description;
+    }
+
+    private String help()
+    {
+        StringBuilder message = new StringBuilder();
+        commands.forEach((commandName, command) -> {
+            message.append(commandName);
+            message.append("\t\t");
+            message.append(command.description());
+            message.append("\n");
+        });
+
+        message.deleteCharAt(message.length() - 1);
+        return message.toString();
+    }
+}
